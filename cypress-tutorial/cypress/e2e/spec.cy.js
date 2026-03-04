dcf80043c3ad2662f0ee970574420e531ef82f4d
Milena-Gryszkin <87983339+Milena-Gryszkin@users.noreply.github.com>
2026-03-04 10:38:16 +0100

    cypress-tutorial

---
+++ cypress-tutorial/cypress/e2e/spec.cy.js
@@ -0,0 +1,26 @@
+
+/// <reference types="Cypress" />
+
+describe('My first test', () => {
+  it('Gets, types, and asserts', () => {
+    //Arrange 
+
+    cy.visit('https://example.cypress.io')
+cy.contains('type').click()
+cy.url().should('include', '/commands/actions')
+
+//Act
+cy.get('.action-email').type('fake@email.com')
+
+ //Asserts
+  cy.get('.action-email').should('contain.value', '@email.com')
+  })
+  
+})
+describe('My second test', () => {
+  it('Localhost', () => {
+
+    cy.visit('http://localhost:3000')
+  })
+  
+})
