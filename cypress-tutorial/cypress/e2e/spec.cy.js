e3933929cbaf2317a4774b5cbaf583e497f38d3f
Milena-Gryszkin <87983339+Milena-Gryszkin@users.noreply.github.com>
2026-03-04 15:39:54 +0100

    Update spec.cy.js

--- cypress-tutorial/cypress/e2e/spec.cy.js
+++ cypress-tutorial/cypress/e2e/spec.cy.js
@@ -1,26 +1,32 @@
-
 /// <reference types="Cypress" />
 
-describe('My first test', () => {
-  it('Gets, types, and asserts', () => {
-    //Arrange 
+describe("My first test", () => {
+  it("Gets, types, and asserts", () => {
+    //Arrange
 
-    cy.visit('https://example.cypress.io')
-cy.contains('type').click()
-cy.url().should('include', '/commands/actions')
+    cy.visit("https://example.cypress.io");
+    cy.contains("type").click();
+    cy.url().should("include", "/commands/actions");
 
-//Act
-cy.get('.action-email').type('fake@email.com')
+    //Act
+    cy.get(".action-email").type("fake@email.com");
 
- //Asserts
-  cy.get('.action-email').should('contain.value', '@email.com')
-  })
-  
-})
-describe('My second test', () => {
-  it('Localhost', () => {
-    cy.visit('http://localhost:3000')
-    cy.get("h1")
-  })
-  
-})
+    //Asserts
+    cy.get(".action-email").should("contain.value", "@email.com");
+  });
+});
+describe("My second test", () => {
+  it("Localhost", () => {
+    cy.log("Testing localhost");
+    cy.visit("http://localhost:3000");
+    cy.get("[data-test='hero-heading']").contains(
+      "Testing Next.js Applications with Cypress",
+    );
+    //h1[data-test='hero-heading']
+  });
+  it("the features on the homepage are correct", () => {
+    cy.visit("http://localhost:3000");
+    cy.get("dt").eq(0).contains("4 Courses");
+    cy.get("dt").eq(1).contains("25+ Lessons");
+  });
+});
