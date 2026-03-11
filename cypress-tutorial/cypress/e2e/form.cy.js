85be96dec81c3951c250f1e12ee509facb6e719a
Milena-Gryszkin <87983339+Milena-Gryszkin@users.noreply.github.com>
2026-03-11 15:47:48 +0100

    New test- continuation of Cypress course

--- cypress-tutorial/cypress/e2e/form.cy.js
+++ cypress-tutorial/cypress/e2e/form.cy.js
@@ -3,18 +3,25 @@ describe("Newsletter Subscribe Form", () => {
     cy.visit("http://localhost:3000");
   });
 
-  it("Testing invalid email", () => {
-    cy.getByData("email-input").type("invalidMail");
-    cy.get('[data-test="submit-button"]').click();
+  context("Form Validation", () => {
+    it("Testing invalid email", () => {
+      cy.getByData("email-input").type("invalidMail");
+      cy.get('[data-test="submit-button"]').click();
+    });
+    it("Testing valid email", () => {
+      cy.getByData("email-input").type("validMail@example.com");
+      cy.get('[data-test="submit-button"]').click();
+      cy.get('[data-test="success-message"]').should("be.visible");
+      cy.get('[data-test="email-input"]').should(
+        "have.attr",
+        "placeholder",
+        "Subscribe for Updates",
+      );
+    });
   });
-  it("Testing valid email", () => {
-    cy.getByData("email-input").type("validMail@example.com");
-    cy.get('[data-test="submit-button"]').click();
-    cy.get('[data-test="success-message"]').should("be.visible");
-    cy.get('[data-test="email-input"]').should(
-      "have.attr",
-      "placeholder",
-      "Subscribe for Updates",
-    );
+  context("Button", () => {
+    it.only("Course:Testing Your First Next.js Application", () => {
+      cy.getByData("course-0").find("a").eq(3).click();
+    });
   });
 });
