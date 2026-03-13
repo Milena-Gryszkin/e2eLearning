bdc946fb4a649437581de8e47aa222644830f624
Milena-Gryszkin <87983339+Milena-Gryszkin@users.noreply.github.com>
2026-03-13 09:39:44 +0100

    correct flow- second course

--- cypress-tutorial/cypress/e2e/flow.cy.js
+++ cypress-tutorial/cypress/e2e/flow.cy.js
@@ -1,9 +1,10 @@
 describe("Testing Foundations", () => {
   it("Testing is a Mindset", () => {
     cy.visit("/");
-    cy.getByData("lesson-0").should("contain", "Testing is a Mindset");
-    cy.getByData("lesson-upcoming-0").eq(0).click();
-    cy.get('[data-test="course-0"]').find("a").contains("Get started").click();
+    cy.getByData("lesson-progress-link-0")
+      .should("contain", "Testing is a Mindset")
+      .eq(1)
+      .click();
     cy.location("pathname").should(
       "equal",
       "/testing-foundations/testing-is-a-mindset",
